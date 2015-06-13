0dcf3c7770548ab6dd7396dc34aa53c9ab12468d
mjhead <andremjhead@gmail.com>
2015-06-13 16:54:48 +0300

    ADD: TGM plugin activator to install without Wizard

--- functions.php
+++ functions.php
@@ -123,53 +123,162 @@ function cherry_child_register_plugins( $plugins ) {
 			'source'  => 'wordpress'
 		),
 		'cherry-shortcodes' => array(
-			'name'         => 'Cherry Shortcodes', 
-			'slug'         => 'cherry-shortcodes', 
+			'name'         => 'Cherry Shortcodes',
+			'slug'         => 'cherry-shortcodes',
 			'source'       => 'cherry-free'
 		),
 		'cherry-shortcodes-templater' => array(
-			'name'         => 'Cherry shortcodes templater', 
-			'slug'         => 'cherry-shortcodes-templater', 
+			'name'         => 'Cherry shortcodes templater',
+			'slug'         => 'cherry-shortcodes-templater',
 			'source'       => 'cherry-free'
 		),
 		'cherry-simple-slider' => array(
-			'name'         => 'Cherry simple slider', 
-			'slug'         => 'cherry-simple-slider', 
+			'name'         => 'Cherry simple slider',
+			'slug'         => 'cherry-simple-slider',
 			'source'       => 'cherry-free'
 		),
 		'cherry-portfolio' => array(
-			'name'         => 'Cherry portfolio', 
-			'slug'         => 'cherry-portfolio', 
+			'name'         => 'Cherry portfolio',
+			'slug'         => 'cherry-portfolio',
 			'source'       => 'cherry-free'
 		),
 		'cherry-testimonials' => array(
-			'name'         => 'Cherry testimonials', 
-			'slug'         => 'cherry-testimonials', 
+			'name'         => 'Cherry testimonials',
+			'slug'         => 'cherry-testimonials',
 			'source'       => 'cherry-free'
 		),
 		'cherry-team' => array(
-			'name'         => 'Cherry team', 
-			'slug'         => 'cherry-team', 
+			'name'         => 'Cherry team',
+			'slug'         => 'cherry-team',
 			'source'       => 'cherry-free'
 		),
 		'cherry-social' => array(
-			'name'         => 'Cherry social', 
-			'slug'         => 'cherry-social', 
+			'name'         => 'Cherry social',
+			'slug'         => 'cherry-social',
 			'source'       => 'cherry-free'
 		),
 		'cherry-grid' => array(
-			'name'         => 'Cherry grid', 
-			'slug'         => 'cherry-grid', 
+			'name'         => 'Cherry grid',
+			'slug'         => 'cherry-grid',
 			'source'       => 'cherry-free'
 		),
 		'cherry-charts' => array(
-			'name'         => 'Cherry charts', 
-			'slug'         => 'cherry-charts', 
+			'name'         => 'Cherry charts',
+			'slug'         => 'cherry-charts',
 			'source'       => 'cherry-free'
 		)
 	);
 
 	return $plugins;
+}
+
+// register plugin for TGM activator
+require_once get_stylesheet_directory() . '/inc/class-tgm-plugin-activation.php';
+add_action( 'tgmpa_register', 'cherryone_register_plugins' );
+function cherryone_register_plugins() {
+
+	/**
+	 * Array of plugin arrays. Required keys are name and slug.
+	 */
+	$plugins = array(
+		array(
+			'name'     => 'Contact Form 7',
+			'slug'     => 'contact-form-7',
+			'required' => true
+		),
+		array(
+			'name'         => 'Cherry Testimonials',
+			'slug'         => 'cherry-testimonials',
+			'source'       => 'https://api.github.com/repos/CherryFramework/cherry-testimonials/zipball/',
+			'required'     => true,
+			'external_url' => 'https://github.com/CherryFramework/cherry-testimonials',
+		),
+		array(
+			'name'         => 'Cherry Team',
+			'slug'         => 'cherry-team',
+			'source'       => 'https://api.github.com/repos/CherryFramework/cherry-team/zipball/',
+			'required'     => true,
+			'external_url' => 'https://github.com/CherryFramework/cherry-team',
+		),
+		array(
+			'name'         => 'Cherry Social',
+			'slug'         => 'cherry-social',
+			'source'       => 'https://api.github.com/repos/CherryFramework/cherry-social/zipball/',
+			'required'     => true,
+			'external_url' => 'https://github.com/CherryFramework/cherry-social',
+		),
+		array(
+			'name'         => 'Cherry Simple Slider',
+			'slug'         => 'cherry-simple-slider',
+			'source'       => 'https://api.github.com/repos/CherryFramework/cherry-simple-slider/zipball/',
+			'required'     => true,
+			'external_url' => 'https://github.com/CherryFramework/cherry-simple-slider',
+		),
+		array(
+			'name'         => 'Cherry Shortcodes Templater',
+			'slug'         => 'cherry-shortcodes-templater',
+			'source'       => 'https://api.github.com/repos/CherryFramework/cherry-shortcodes-templater/zipball/',
+			'required'     => true,
+			'external_url' => 'https://github.com/CherryFramework/cherry-shortcodes-templater',
+		),
+		array(
+			'name'         => 'Cherry Shortcodes',
+			'slug'         => 'cherry-shortcodes',
+			'source'       => 'https://api.github.com/repos/CherryFramework/cherry-shortcodes/zipball/',
+			'required'     => true,
+			'external_url' => 'https://github.com/CherryFramework/cherry-shortcodes',
+		),
+		array(
+			'name'         => 'Cherry Portfolio',
+			'slug'         => 'cherry-portfolio',
+			'source'       => 'https://api.github.com/repos/CherryFramework/cherry-portfolio/zipball/',
+			'required'     => true,
+			'external_url' => 'https://github.com/CherryFramework/cherry-portfolio',
+		),
+		array(
+			'name'         => 'Cherry Charts',
+			'slug'         => 'cherry-charts',
+			'source'       => 'https://api.github.com/repos/CherryFramework/cherry-charts/zipball/',
+			'required'     => true,
+			'external_url' => 'https://github.com/CherryFramework/cherry-charts',
+		)
+	);
+
+	/**
+	 * Array of configuration settings. Amend each line as needed.
+	 */
+	$config = array(
+		'default_path' => '',                      // Default absolute path to pre-packaged plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+		'strings'      => array(
+			'page_title'                      => __( 'Install Required Plugins', 'tgmpa' ),
+			'menu_title'                      => __( 'Install Plugins', 'tgmpa' ),
+			'installing'                      => __( 'Installing Plugin: %s', 'tgmpa' ), // %s = plugin name.
+			'oops'                            => __( 'Something went wrong with the plugin API.', 'tgmpa' ),
+			'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ), // %1$s = plugin name(s).
+			'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ), // %1$s = plugin name(s).
+			'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s).
+			'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+			'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+			'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), // %1$s = plugin name(s).
+			'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), // %1$s = plugin name(s).
+			'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s).
+			'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
+			'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins' ),
+			'return'                          => __( 'Return to Required Plugins Installer', 'tgmpa' ),
+			'plugin_activated'                => __( 'Plugin activated successfully.', 'tgmpa' ),
+			'complete'                        => __( 'All plugins installed and activated successfully. %s', 'tgmpa' ), // %s = dashboard link.
+			'nag_type'                        => 'updated'
+		)
+	);
+
+	tgmpa( $plugins, $config );
+
 }
 
 /**
