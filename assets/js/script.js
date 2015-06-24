1bc8e837c8a103854cd69d53005203c8d7263f82
mjhead <andremjhead@gmail.com>
2015-06-24 16:19:27 +0300

    UPD: banner CSS fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,33 @@
 jQuery(document).ready(function(){
     
+    var _window = jQuery(window);
+    var same_height_objs = jQuery('.same-height');
+    
+    set_same_height();
+    _window.resize(set_same_height);
+    
+    function set_same_height()
+    {
+        same_height_objs.css({height:'auto'});
+        same_height_objs.height(get_max_height());
+        
+        function get_max_height()
+        {
+            max_height = 0;
+            
+            same_height_objs.each(function ()
+            {
+                this_height = jQuery(this).height();
+                if(this_height > max_height) max_height =  this_height;
+            });
+            
+            return max_height;
+        }
+    }
+    
+    
+    
+    
     var searchHolder = jQuery('#header .search-form');
 	var searchButton = jQuery('input[type="submit"]', searchHolder);
 	var searchInput = jQuery('input[type="search"]', searchHolder);
